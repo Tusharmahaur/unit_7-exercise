8ddde06993c961378f7c6eb3892eca6fd52f5ce7
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-12 10:51:17 +0530

    this

--- overloadingFourArithmeticOperator.cpp
+++ overloadingFourArithmeticOperator.cpp
@@ -5,5 +5,47 @@ class Float{
     private:
         float data;
     public:
-        
+        Float(float d){
+            data = d;
+        }    
+        Float operator+(Float f)/*{
+            return Float(data + f.data);
+        }*/;
+        Float operator-(Float f){
+            return Float(data - f.data);
+        }
+        Float operator*(Float f){
+            return Float(data * f.data);
+        }
+        Float operator/(Float f){
+            return Float(data / f.data);
+        }
+        void display(){
+            cout << data << endl;
+        }
+};
+
+Float Float::operator+(Float f){
+    Float temp = Float(data + f.data);
+    return temp;
+}
+
+int main(){
+    Float f1(10.2);
+    Float f2(2.0);
+    Float f3(0.0);
+    f3 = f1 + f2;
+    cout << "Addition: ";
+    f3.display();
+    f3 = f1 - f2;
+    cout << "Subtraction: ";
+    f3.display();
+    f3 = f1 * f2;
+    cout << "Multiplication: ";
+    f3.display();
+    f3 = f1 / f2;
+
+    cout << "Division: ";
+    f3.display();
+    return 0;
 }
