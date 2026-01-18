a8c64920f24d1d02b587fe394a0821e7a1478f3e
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-18 09:54:00 +0530

    this

---
+++ polarCoordinate.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Polar{
+//     private:
+//         float radius,angle;
+//     public:
+//         //constructor for operator overloading 
+//         Polar(float radius,float angle){
+//             this ->radius = radius;
+//             this ->angle = angle;
+//         }
+        
+//         //operator overloading for +
+//         Polar operator+(Polar);
+//         void display(void);
+
+// };
+
+// Polar Polar::operator+(Polar p){
+//     Polar object;
+    
+// }
+// int main(){
+    
+//     return 0;
+// }
+
