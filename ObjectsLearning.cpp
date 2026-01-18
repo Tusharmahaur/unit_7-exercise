59dcf3d955387ddd2bf05e6a3b296968d99dbdbc
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-18 17:27:01 +0530

    this

--- ObjectsLearning.cpp
+++ ObjectsLearning.cpp
@@ -690,3 +690,54 @@
 //     }
 //     return 0;
 // }
+
+//add two matrix default parameter 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int matrix1[2][2] = {{1,2},
+//                         {3,4}};
+//     int matrix2[2][2] = {{4,5},{6,8}};
+//     //iputting matrix 
+//     // cout<<"------------Inputting for matrix1-----------------"<<endl;
+//     // for(int i= 0; i<2;i++){
+//     //     for(int j = 0;j<2;j++){
+//     //         cin>>matrix1[i][j];
+//     //     }
+//     // }
+
+//     // cout<<"------------Inputting for matrix2-----------------"<<endl;
+//     // for(int i= 0; i<2;i++){
+//     //     for(int j = 0;j<2;j++){
+//     //         cin>>matrix2[i][j];
+//     //     }
+//     // }
+
+//     //outputting matrix
+//     cout<<"---------------outputting for matrix 1-------------------"<<endl;
+//     for(int i = 0;i<2;i++){
+//         for(int j=0;j<2;j++){
+//             cout<<matrix1[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//     cout<<"-------------outputting for Matrix 2--------------------"<<endl;
+//     for(int i = 0;i<2;i++){
+//         for(int j=0;j<2;j++){
+//             cout<<matrix2[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//     cout<<"----------------Adding the two matrix--------------------"<<endl;
+//     for(int i = 0; i<2; i++){
+//         for(int j = 0; j<2; j++){
+//             cout<<matrix1[i][j] + matrix2[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
